9ae6dfb0f5ea37b2730fe2a789cf05eaf4dad86e
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-02-16 14:02:24 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿namespace HelloWorld
+﻿using System.Text;
+using System.IO;
+namespace HelloWorld
 {
     class Program
     {
+        //Lista dinamikusan változó méretű tömb
+        //Listában tárolunk
+        static List<MoHegyei> MoHegyei_List = new List<MoHegyei>();
         static void Main(string[] args)
         {
             //Magyarország hegyei, soronkénti beolvasás
